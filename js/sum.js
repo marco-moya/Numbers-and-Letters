37db6ecf8e9add50079ea36d752b51789d30b0cb
Marco <zgmf_x235@hotmail.com>
2025-04-13 17:10:29 -0600

    Add animation to the result and a modal editing window in sum.js - Improved validation of values in inputs. - Added modal window to change the dimensions of math operation. - Added animation to validate the result.

--- js/sum.js
+++ js/sum.js
@@ -2,40 +2,143 @@ const d = document;
 
 export function sum() {
   const sumForm = d.querySelector(".form-sum-container");
-  const $addend1 = d.getElementById("addend1");
-  const $addend2 = d.getElementById("addend2");
+  const $addendContainer = d.querySelector(".addend-container");
+  const $inputSumContainer = d.querySelector(".input-sum-container");
   const $sumResult = d.getElementById("sum-result");
-  const $btnSumResult = d.getElementById("btn-sum-result");
+  const $valueColumn = d.getElementById("value-column");
+  const $valueRow = d.getElementById("value-row");
 
-  let addend1;
-  let addend2;
+  let column = 4;
+  let row = 2;
+  let sumTotal;
+  let userSumTotal;
+  
   function resetAddends() {
-    addend1 = Math.floor(Math.random() * 9000) + 1000;
-    addend2 = Math.floor(Math.random() * 9000) + 1000;
-    console.log(addend1, addend2);
-    $addend1.textContent = addend1;
-    $addend2.textContent = addend2;
+    sumTotal = 0;
+    $addendContainer.innerHTML = "";
+    $inputSumContainer.innerHTML = "";
+    for (let i = 0; i < row; i++) {
+      const $label = d.createElement("label");
+      $label.setAttribute("id", `addend${i + 1}`);
+      $label.textContent = Math.floor(Math.random() * 9 * Math.pow(10, column-1)) + Math.pow(10, column-1);
+      $addendContainer.appendChild($label);
+      sumTotal += parseInt($label.textContent);
+    }
+    for (let i = 0; i <= column; i++) {
+      const $input = d.createElement("input");
+      $input.setAttribute("type", "number");
+      $input.setAttribute("id", `addend${i + 1}`);
+      $input.setAttribute("min", `${0}`);
+      $input.setAttribute("max", `${9}`);
+      $inputSumContainer.appendChild($input);
+    }
+    const $span = d.createElement("span");
+    $span.classList.add("plus-sign");
+    $span.textContent = "+";
+    $addendContainer.appendChild($span);
   }
 
   function sumAddends() {
-    $sumResult.textContent = addend1 + addend2;
-    console.log(addend1 + addend2);
+    $sumResult.textContent = sumTotal;
+  }
+
+  function evaluateSum() {
+    const inputValues = [];
+    const $inputsSum = d.querySelectorAll("input[type='number']");
+    $inputsSum.forEach((input) => {
+      if (input.value == "") input.value = 0;
+      inputValues.push(input.value);
+    });
+    userSumTotal = parseInt(inputValues.reduce((acc, val) => acc + val , 0));
+    if (userSumTotal === sumTotal) {
+      showCorrectResult();
+    } else {
+      showIncorrectResult();
+    }
+  }
+
+  function showCorrectResult() {
+    d.querySelector(".container-true").classList.toggle("is-active");
+    setTimeout(() => {
+      d.querySelector(".container-true").classList.toggle("is-active")
+    }, 800);
+    const $alarmCorrect = d.createElement("audio");
+    $alarmCorrect.src = "../assets/correct_choice.mp3";
+    $alarmCorrect.play();
+  }
+
+  function showIncorrectResult() {
+    d.querySelector(".container-false").classList.toggle("is-active");
+    setTimeout(() => {
+      d.querySelector(".container-false").classList.toggle("is-active")
+    }, 800);
+    const $alarmIncorrect = d.createElement("audio");
+    $alarmIncorrect.src = "../assets/negative_choice.mp3";
+    $alarmIncorrect.play();
   }
   
+  $valueColumn.textContent = column;
+  $valueRow.textContent = row;
   resetAddends();
+  
+  $inputSumContainer.addEventListener("input", (e) => {
+    if (e.target.matches("input[type='number']")) {
+      const value = e.target.value;
+      e.target.value = value.slice(-1);
+    }
+  });
 
   sumForm.addEventListener("submit", (e) => {
     e.preventDefault();
   });
 
   d.addEventListener("click", (e) => {
-    if (e.target.matches("#btn-sum-result") || e.target.matches("#btn-sum-result i")) {
+    if (e.target.matches("#btn-sum-result")) {
       sumAddends();
       $sumResult.classList.remove("hidden");
+      evaluateSum();
+      d.getElementById("btn-sum-result").disabled = true;
+      d.getElementById("btn-sum-reset").disabled = false;
     }
-    if (e.target.matches("#btn-reset-sum") || e.target.matches("#btn-reset-sum i")) {
+    if (e.target.matches("#btn-sum-reset") || e.target.matches("#btn-sum-reset i")) {
       resetAddends();
       $sumResult.classList.add("hidden");
+      d.getElementById("btn-sum-result").disabled = false;
+      d.getElementById("btn-sum-reset").disabled = true;
+    }
+    if (e.target.matches("#btn-sum-edit") || e.target.matches("#btn-sum-edit i")) {
+      d.querySelector(".modal").classList.add("show-modal");
+    }
+    if (e.target.matches("#btn-sum-edit-close")) {
+      d.querySelector(".modal").classList.remove("show-modal");
+    }
+    if (e.target.matches("#btn-increase-colum") || e.target.matches("#btn-increase-colum i")) {
+      if ($valueColumn.textContent < 9) {
+        column++;
+        $valueColumn.textContent = column;
+        resetAddends();
+      }
+    }
+    if (e.target.matches("#btn-decrease-colum") || e.target.matches("#btn-decrease-colum i")) {
+      if ($valueColumn.textContent > 1) {
+        column--;
+        $valueColumn.textContent = column;
+        resetAddends();
+      }
+    }
+    if (e.target.matches("#btn-increase-row") || e.target.matches("#btn-increase-row i")) {
+      if ($valueRow.textContent < 9) {
+        row++;
+        $valueRow.textContent = row;
+        resetAddends();
+      }
+    }
+    if (e.target.matches("#btn-decrease-row") || e.target.matches("#btn-decrease-row i")) {
+      if ($valueRow.textContent > 2) {
+        row--;
+        $valueRow.textContent = row;
+        resetAddends();
+      }
     }
   });
 
